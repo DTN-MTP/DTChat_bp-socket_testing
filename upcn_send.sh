773897436afb3f6f40e6d6c740d08a56badcdb31
MalekkJam <malekjamel007@gmail.com>
2025-06-13 11:42:01 +0200

    Added A-SABR submodule

--- upcn_send.sh
+++ upcn_send.sh
@@ -1,4 +1,4 @@
-# sending to ipn:10.2 
+# sending to ipn:10.2
 
 cd /home/vagrant/ud3tn/
 source .venv/bin/activate
@@ -8,4 +8,4 @@ python3 tools/aap2/aap2_send.py --agentid 2 --socket ./ud3tn.aap2.socket.2 ipn:1
 
 cd /home/vagrant/ud3tn/
 source .venv/bin/activate
-python3 tools/aap2/aap2_send.py --agentid 2 --socket ./ud3tn.aap2.socket.2 ipn:30.2 "Hello from ud3tn!" -v
+python3 tools/aap2/aap2_send.py --agentid 2 --socket ./ud3tn.aap2.socket.2 ipn:30.2 "Hello from ud3tn!" -v  
