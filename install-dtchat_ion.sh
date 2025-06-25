5a3f91f9bdfc3cd834d754c5c51c875974c03708
Olivier De Jonckere <olivierdejonckere@gmail.com>
2025-06-25 11:14:52 +0200

    Update subs

--- install-dtchat_ion.sh
+++ install-dtchat_ion.sh
@@ -6,3 +6,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 . "$HOME/.cargo/env"
 apt install -y protobuf-compiler libxkbcommon-x11-0
 sed -i 's/PLACEHOLDER_BP_ADDR/ipn:10.2/g' database.yaml
+sed -i 's/host2.rc/host10.rc/g' database.yaml
